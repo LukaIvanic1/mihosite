2de7345c2084f8b8229539da1916c393299a3e24
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2023-01-11 21:53:55 +0100

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    // Get the text field
+    var copyText = "michaelprobably@gmail.com"
+  
+    
+
+  
+
+    navigator.clipboard.writeText(copyText);
+  
+
+    alert("Copied my email address! :)");
+  }
